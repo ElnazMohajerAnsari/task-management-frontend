b92407f11b3dd7ffcc684dbf591b4b5e4a3ec1e2
ElnazMohajerAnsari <103626555+ElnazMohajerAnsari@users.noreply.github.com>
2022-09-03 02:23:30 +0430

    Update header.js
    Adding logout link

--- src/components/header.js
+++ src/components/header.js
@@ -1,17 +1,40 @@
 import { Link } from "react-router-dom";
 
+var link;
+if (localStorage.getItem("user") === "Admin") {
+  link = (
+    <Link
+      className="logout-link"
+      to={"/"}
+      type="button"
+      onClick={() => {
+        handleLogOut();
+        window.location.href = "/";
+      }}
+    >
+      Logout
+    </Link>
+  );
+} else {
+  link = (
+    <h5 className="login-link">
+      Are you an admin?{" "}
+      <span>
+        <Link to={"/login"}>Login</Link>{" "}
+      </span>
+    </h5>
+  );
+}
+
+const handleLogOut = () => {
+  localStorage.setItem("user", "User");
+};
+
 const Header = () => {
   return (
     <div className="header">
       <h2>Task Management</h2>
-      <h5 className="login-link">
-        Are you an admin?{" "}
-        <span>
-          <Link to={"/login"}>
-            Login
-          </Link>{" "}
-        </span>
-      </h5>
+      {link}
     </div>
   );
 };
